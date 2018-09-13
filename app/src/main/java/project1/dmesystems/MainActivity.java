6f08306cdfae259c8cf0f61b93310db232814155
LeonelMa17 <43215474+LeonelMa17@users.noreply.github.com>
2018-09-13 00:53:01 -0500

    Update MainActivity.java

--- app/src/main/java/project1/dmesystems/MainActivity.java
+++ app/src/main/java/project1/dmesystems/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+   //Declaramos nuestras variables de tipo vistas para que sea mas facil el manejo de nuestros recursos
     RatingBar barra_calificar;
     Button boton_calificar;
 
@@ -15,16 +16,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     //Mandamos llamar al metodo de boton
         eventoBoton();
 
     }
 
+    //Usamos el metodo de eventoBoton para calificar nuestro negocio
     public void eventoBoton() {
+     //Buscamos nuestros recursos con el metodo findview para las estrellas y para el boton de calificar
         barra_calificar = (RatingBar) findViewById(R.id.barra_estrellas);
         boton_calificar = (Button) findViewById(R.id.boton_calificar);
         boton_calificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+             //Al momento de calificar con el evento del boton ejecuta el metodo onClick, donde mandamos un mensaje
+             //dando las gracias por calificar la aplicación y la mostramos
                 String no_strellas = "gracias por calificar con "
                         + String.valueOf(barra_calificar.getRating())
                         + " estrellas a esta aplicacion.";
